93bffdb3bbe074748782d72ef857e37b295eddbe
大里匠 <dolphin.rokujiro@gmail.com>
2022-11-18 11:02:03 +0900

    lint時にPrettierのformatが実行されないよう変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { createGlobalStyle } from 'styled-components'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 html,
@@ -30,7 +30,7 @@ a {
   }
 }
 
-`
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -48,5 +48,5 @@ export default function App({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
